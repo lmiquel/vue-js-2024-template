60460d268c511bd1929572f2c3ee8c14d4d321dd
Léa Miquel <leamiquel21@gmail.com>
2024-10-23 21:55:08 +0200

    vue-js-2024-template

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+
+import { createApp } from 'vue'
+import { createVuetify } from 'vuetify'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
