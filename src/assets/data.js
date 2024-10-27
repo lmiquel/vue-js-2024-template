0fc1c0d6ec22f352b3ec810e20681149dca1141f
lmiquel <leamiquel21@gmail.com>
2024-10-27 15:07:26 +0000

    livecoding

---
+++ src/assets/data.js
@@ -0,0 +1,125 @@
+export const books = [
+  {
+    author: 'Jane Austen',
+    title: 'Pride and Prejudice',
+    year: 1813,
+  },
+  {
+    author: 'Honoré de Balzac',
+    title: 'Le Père Goriot',
+    year: 1835,
+  },
+  {
+    author: 'Samuel Beckett',
+    title: 'Molloy, Malone Dies, The Unnamable, the trilogy',
+    year: 1952,
+  },
+  {
+    author: 'Giovanni Boccaccio',
+    title: 'The Decameron',
+    year: 1351,
+  },
+  {
+    author: 'Jorge Luis Borges',
+    title: 'Ficciones',
+    year: 1965,
+  },
+  {
+    author: 'Emily Brontë',
+    title: 'Wuthering Heights',
+    year: 1847,
+  },
+  {
+    author: 'Albert Camus',
+    title: 'The Stranger',
+    year: 1942,
+  },
+  {
+    author: 'Paul Celan',
+    title: 'Poems',
+    year: 1952,
+  },
+  {
+    author: 'Louis-Ferdinand Céline',
+    title: 'Journey to the End of the Night',
+    year: 1932,
+  },
+  {
+    author: 'Miguel de Cervantes',
+    title: 'Don Quijote De La Mancha',
+    year: 1610,
+  },
+  {
+    author: 'Geoffrey Chaucer',
+    title: 'The Canterbury Tales',
+    year: 1450,
+  },
+  {
+    author: 'Anton Chekhov',
+    title: 'Stories',
+    year: 1886,
+  },
+  {
+    author: 'Joseph Conrad',
+    title: 'Nostromo',
+    year: 1904,
+  },
+  {
+    author: 'Charles Dickens',
+    title: 'Great Expectations',
+    year: 1861,
+  },
+  {
+    author: 'Denis Diderot',
+    title: 'Jacques the Fatalist',
+    year: 1796,
+  },
+]
+
+export const songs = [
+  {
+    title: 'Death Bed',
+    artist: 'Powfu',
+    year: 2020,
+  },
+  {
+    title: 'Bad Liar',
+    artist: 'Imagine Dragons',
+    year: 2018,
+  },
+  {
+    title: 'Faded',
+    artist: 'Alan Walker',
+    year: 2015,
+  },
+  {
+    title: 'Hate Me',
+    artist: 'Ellie Goulding',
+    year: 2019,
+  },
+  {
+    title: 'Solo',
+    artist: 'Clean Bandit',
+    year: 2018,
+  },
+  {
+    title: 'Without Me',
+    artist: 'Halsey',
+    year: 2018,
+  },
+  {
+    title: '360',
+    artist: 'Charli XCX',
+    year: 2024,
+  },
+  {
+    title: 'Marry the Night',
+    artist: 'Lady Gaga',
+    year: 2011,
+  },
+  {
+    title: 'Master of Puppets',
+    artist: 'Metallica',
+    year: 1986,
+  },
+]
