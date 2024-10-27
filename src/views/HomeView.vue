f7b7dc237b68a0007da80ca06369a866f7a50e7a
lmiquel <leamiquel21@gmail.com>
2024-10-27 16:45:26 +0000

    exo-router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,8 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
+  <p>
+    Lorsqu'on clique sur Adidas, les informations d'Adidas doivent apparaître
+  </p>
+  <p>Lorsqu'on clique sur Nike, les informations de Nike doivent apparaître</p>
 </template>
